a4f005f33c9515dc784b1e18c827947b110930a2
dindin-abdulloh <dindin.abduloh11@gmail.com>
2025-12-23 08:37:59 +0700

    fix route

--- resources/views/components/app/sidebar.blade.php
+++ resources/views/components/app/sidebar.blade.php
@@ -175,7 +175,7 @@
                             <ul class="pl-8 mt-1 @if(!in_array(Request::segment(1), ['dashboard'])){{ 'hidden' }}@endif" :class="open ? 'block!' : 'hidden'">
 
                                 <li class="mb-1 last:mb-0">
-                                    <a class="block text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 transition truncate  href="https://sites.google.com/view/komper-rsmb/home">
+                                    <a class="block text-gray-500/90 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-200 transition truncate @if(Route::is('employee-non-permanent')){{ 'text-violet-500!' }}@endif" href="https://sites.google.com/view/komper-rsmb/home">
                                         <span class="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">Logbook</span>
                                     </a>
                                 </li>
